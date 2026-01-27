f707b262b4c06f98024d52c0cb6744de2154d0d1
SumitSh7 <sumitsh2012@gmail.com>
2026-01-27 18:38:47 +0100

    More Trees part 2

---
+++ max_depth.py
@@ -0,0 +1,39 @@
+from typing import Optional
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def maxDepth_dfs(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        left_height = self.maxDepth(root.left)
+        right_height = self.maxDepth(root.right)
+        
+        return 1 + max(left_height, right_height)
+    
+    def maxDepth_bfs(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        queue = deque([root])
+        level = 0
+        
+        while queue:
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            level += 1
+            
+        return level
---
+++ min_depth.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+        
+        queue = deque([root])
+        level = 1
+
+        while queue:
+            for _ in range(len(queue)):
+                
+                node = queue.popleft()
+
+                if not node.left and not node.right:
+                    return level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:    
+                    queue.append(node.right)
+            level += 1    
+            
+        return level
